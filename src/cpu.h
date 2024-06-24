fe25717b7b6804bdf6af1e382bd74471b8a7588e
infrx <mertcansunuc@gmail.com>
2024-06-24 23:01:40 +0300

    Update progress

--- src/cpu.h
+++ src/cpu.h
@@ -1,1 +1,17 @@
 #pragma once
+#include <cstdint>
+#include <iostream>
+class Cpu
+{
+public:
+	uint8_t opcode{0};
+	uint8_t memory[65536];
+
+	void executeOpcode();
+	void executeCBOpcode();
+private:
+	uint8_t mem_read(const uint64_t adr);
+	void mem_write(const uint64_t adr, uint8_t value);
+
+
+};
--- src/register.h
+++ src/register.h
@@ -1,1 +1,43 @@
 #pragma once
+
+#include <cstdint>
+#include <iostream>
+
+class Registers
+{
+public:
+
+	uint16_t pc{};
+	uint16_t sp{};
+    union {
+        struct {
+            uint8_t f;   // Flags register
+            uint8_t a;   // Accumulator register
+        };
+        uint16_t af;     // Combined AF register (f + a)
+    };
+
+    union {
+        struct {
+            uint8_t c;   // Counter register
+            uint8_t b;   // B register
+        };
+        uint16_t bc;     // Combined BC register (c + b)
+    };
+
+    union {
+        struct {
+            uint8_t e;   // E register
+            uint8_t d;   // D register
+        };
+        uint16_t de;     // Combined DE register (e + d)
+    };
+
+    union {
+        struct {
+            uint8_t l;   // L register
+            uint8_t h;   // H register
+        };
+        uint16_t hl;     // Combined HL register (l + h)
+    };
+};
