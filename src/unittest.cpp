4d90b821ef4e5cc134abea59523af96b0c557373
Infrx <mertcansunuc@gmail.com>
2024-07-15 23:40:49 +0300

    small update

--- src/unittest.cpp
+++ src/unittest.cpp
@@ -1,12 +1,21 @@
 #include <nlohmann/json.hpp>
 #include <fstream>
+#include <iostream>
 
 using json = nlohmann::json;
 
-std::ifstream f("v1/00.json");
-json data;
+
+
 
 int main()
 {
+	std::ifstream f("tests/00.json");
+	if (!f.is_open()) {
+		std::cerr << "Unable to open file!" << std::endl;
+		return 1;
+	}
+	json data = json::parse(f);
+	auto name = data[0]["initial"]["a"];
+	std::cout << name << std::endl;
 	return 0;
 }
