48b969ba2b5913f3a7b3167db4c8749fa284ef94
Mertcan <mertcansunuc@gmail.com>
2024-09-16 23:00:56 +0300

    Started working on mmu

---
+++ src/mmu.cpp
@@ -0,0 +1,5 @@
+//
+// Created by mertcan on 9/16/24.
+//
+
+#include "mmu.h"
---
+++ src/mmu.h
@@ -0,0 +1,16 @@
+//
+// Created by mertcan on 9/16/24.
+//
+
+#ifndef MMU_H
+#define MMU_H
+
+
+
+class Mmu {
+
+};
+
+
+
+#endif //MMU_H
